1c80c01858239834d8856bb0c394cf5bf10f2147
Jihyeok11 <woah68@gmail.com>
2022-03-05 18:53:23 +0900

    1966 풀이 및 해설

--- MakeMoneying/Week 2/1966_프린터큐.py
+++ MakeMoneying/Week 2/1966_프린터큐.py
@@ -1,12 +1,27 @@
 import sys
-from collections import defaultdict
 sys.stdin = open("1966in.txt", 'r')
 
 for _ in range(int(sys.stdin.readline())):
     n, m = map(int, sys.stdin.readline().split())
     basket = list(map(int, sys.stdin.readline().split()))
-    nums = defaultdict(int)
-    for i in basket:
-        nums[i] += 1
-    print(nums)
-    
+    flag = True
+    answer = 0
+    idx = 0
+    while flag:
+        for lvl in range(9, 0, -1):
+            tp = 0
+            for i in range(n):
+                if lvl == basket[(idx + i) % n]:
+                    if basket[(idx + i) % n] > basket[m]:
+                        answer += 1
+                        tp = i
+                    elif (idx + i) % n == m:
+                        answer += 1
+                        flag = False
+                        break
+                    elif basket[(idx + i) % n] == basket[m]:
+                        answer += 1
+            idx = (idx + tp) % n
+            if not flag:
+                break
+    print(answer)
