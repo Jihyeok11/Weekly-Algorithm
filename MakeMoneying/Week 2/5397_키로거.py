297ca83c2e72cb438a29ec130302049d06bd708d
Jihyeok11 <woah68@gmail.com>
2022-03-13 02:09:18 +0900

    5397 풀이 및 해설

--- MakeMoneying/Week 2/5397_키로거.py
+++ MakeMoneying/Week 2/5397_키로거.py
@@ -1,22 +1,25 @@
 import sys
+from collections import deque
+sys.stdin = open("5397in.txt", 'r')
 
 for _ in range(int(sys.stdin.readline())):
-    idx = 0
     words = sys.stdin.readline().strip()
-    answer = ""
-    lenA = 0
+    # 커서를 중심으로 왼쪽과 오른쪽으로 나눔
+    lList = deque([])
+    rList = deque([])
     for w in words:
-        if w == "<":
-            idx = max(0, idx-1)
-        elif w == ">":
-            idx = min(lenA, idx + 1)
+        # ">" 인 경우 커서를 오른쪽으로 옮겼으므로 rList의 맨 앞에 것을 lList로 옮긴다
+        if w == ">":
+            if rList:
+                lList.append(rList.popleft())
+        # "<" 인 경우 커서를 왼쪽으로 옮겼으므로 lList의 가장 끝 값을 rList로 옮긴다
+        elif w == "<":
+            if lList:
+                rList.appendleft(lList.pop())
+        # "-" 인 경우 커서의 앞에 것을 지우므로 lList의 값 하나 제거
         elif w == "-":
-            if idx:
-                answer = answer[:idx-1] +answer[idx:]
-                idx = min(lenA, idx)
-                lenA -= 1
+            if lList:
+                lList.pop()
         else:
-            answer = answer[:idx] + w + answer[idx:]
-            idx += 1
-            lenA += 1
-    print(''.join(answer))
+            lList.append(w)
+    print("".join(lList) + "".join(rList))
