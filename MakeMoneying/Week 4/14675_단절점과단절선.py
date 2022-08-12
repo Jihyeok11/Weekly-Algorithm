beca1789528b9a85310581409a0260ef4dfab7b3
Jihyeok11 <woah68@gmail.com>
2022-08-12 15:15:36 +0900

    22.08.12

--- MakeMoneying/Week 4/14675_단절점과단절선.py
+++ MakeMoneying/Week 4/14675_단절점과단절선.py
@@ -1,8 +1,27 @@
 import sys
 sys.stdin = open("14675in.txt", 'r')
 
-class Nodes:
+class Node:
     def __init__(self, item : int):
         self.item = item
-
-for _ in ra
+        self.canGoTo = []
+rl = sys.stdin.readline
+nodes = {}
+n = int(rl().strip())
+for _ in range(n - 1):
+    a, b = map(int, rl().strip().split())
+    if not a in nodes:
+        nodes[a] = Node(a)
+    if not b in nodes:
+        nodes[b] = Node(b)
+    nodes[a].canGoTo.append(b)
+    nodes[b].canGoTo.append(a)
+for _ in range(int(rl().strip())):
+    t, k = map(int, rl().strip().split())
+    if t == 1:
+        if len(nodes[k].canGoTo) < 2:
+            print("no")
+        else:
+            print("yes")
+    else:
+        print("yes")
